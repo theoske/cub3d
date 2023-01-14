e82215b55f71f7b098e3d4f71447162253e5b03e
Theo Kempf elbaz <tkempf-e@k0r3p12.42mulhouse.fr>
2023-01-14 18:47:22 +0100

    minimap border bug

--- srcs/map/minimap.c
+++ srcs/map/minimap.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:39:01 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/14 16:37:02 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/01/14 18:44:51 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_print_square(int x, int y, mlx_image_t *minimap, uint32_t color)
 	int	j;
 	int	square_size[2];
 
-	square_size[0] = SCREEN_X / 50;
-	square_size[1] = SCREEN_Y / 50;
-	i = square_size[0] * x - 10;
+	square_size[0] = SCREEN_X / 5 / 9;
+	square_size[1] = SCREEN_Y / 5 / 9;
+	i = square_size[0] * x - 9;
 	while (i < (square_size[0] * x))
 	{
-		j = square_size[1] * y - 10;
+		j = square_size[1] * y - 9;
 		while (j < (square_size[1] * y))
 		{
 			mlx_put_pixel(minimap, i, j, color);
@@ -39,10 +39,10 @@ void	ft_mini_init(mlx_image_t *minimap)
 	int	j;
 
 	i = 0;
-	while (i < SCREEN_X / 50 * 9)
+	while (i < SCREEN_X / 5)
 	{
 		j = 0;
-		while (j < SCREEN_X / 50 * 9)
+		while (j < SCREEN_X / 5)
 		{
 			mlx_put_pixel(minimap, i, j, 0x0000ff);
 			j++;
@@ -55,32 +55,31 @@ void	ft_mini_init(mlx_image_t *minimap)
 void	ft_minimap(t_game *g)// pixel noir rend transparent
 {
 	mlx_image_t	*minimap;
+	//coordonnees des points de la map a convertir en minimap
 	int			x;
 	int			y;
+	//coordonnees minimap
 	int			i;
 	int			j;
 
 	minimap = g->img;//minimap 1/5e ecran
 	ft_mini_init(minimap);//met le fond
 	
-	x = /*player - 5*/ g->player->x - 4;
+	x = g->player->x - 4;
 	i = 0;
-	while (i < /*10 cases / taille minimap*/ 9)
+	while (i < 9)
 	{
-		y = /*player + 5*/ g->player->y - 4;
+		y = g->player->y - 4;
 		j = 0;
-		while (j < /*10 cases / taille minimap*/ 9) 
+		while (j < 9) 
 		{
-			printf("x : %d, y : %d\n", x, y);
-			if (x < 0 || y < 0 || !g->map->map[y][x])
+			if (x < 0 || y < 0 || g->map->size_x < x || g->map->size_y < y)
 			{
-				i++;
 				j++;
-				x++;
 				y++;
 				continue;
 			}
-			if (g->map->map[y][x] == '1')
+			else if (x >= 0 && y >= 0 && x < g->map->size_x && y < g->map->size_y && g->map->map[y][x] == '1')
 				ft_print_square(i + 1, j + 1, minimap, 0x2f00ff);
 			y++;
 			j++;
@@ -89,21 +88,4 @@ void	ft_minimap(t_game *g)// pixel noir rend transparent
 		i++;
 	}
 	ft_print_square(5, 5, minimap, 0xff00b7);
-	// i = g->player->x - (SCREEN_X / 200);
-	// printf("x : %f, %d\n", g->player->x, SCREEN_X / 200);
-	// if (i < 0)
-	// 	i = 0;
-	// while (i < g->player->x + (SCREEN_X / 200) && g->map->map[0][i])
-	// {
-	// 	j = g->player->y - (SCREEN_Y / 200);
-	// 	if (j < 0)
-	// 		j = 0;
-	// 	while (j < g->player->y + (SCREEN_Y / 200) && g->map->map[j][0])
-	// 	{
-	// 		if (g->map->map[j][i] == '1')//mur
-	// 			ft_print_square(i, j, minimap, 0x2f00ff);
-	// 		j++;
-	// 	}
-	// 	i++;
-	// }
 }
