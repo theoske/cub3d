d46421020c4252b8714907815eb1a14901f1602d
Theo Kempf elbaz <tkempf-e@k0r3p12.42mulhouse.fr>
2023-01-14 19:56:41 +0100

    minimap border not fixed

--- srcs/map/minimap.c
+++ srcs/map/minimap.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:39:01 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/14 18:44:51 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/01/14 19:55:57 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ void	ft_print_square(int x, int y, mlx_image_t *minimap, uint32_t color)
 
 	square_size[0] = SCREEN_X / 5 / 9;
 	square_size[1] = SCREEN_Y / 5 / 9;
-	i = square_size[0] * x - 9;
-	while (i < (square_size[0] * x))
+	i = square_size[0] * x;
+	while (i < (square_size[0] * x) + square_size[0])
 	{
-		j = square_size[1] * y - 9;
-		while (j < (square_size[1] * y))
+		j = square_size[1] * y;
+		while (j < (square_size[1] * y) + square_size[1])
 		{
 			mlx_put_pixel(minimap, i, j, color);
 			j++;
@@ -64,28 +64,21 @@ void	ft_minimap(t_game *g)// pixel noir rend transparent
 
 	minimap = g->img;//minimap 1/5e ecran
 	ft_mini_init(minimap);//met le fond
-	
-	x = g->player->x - 4;
+	x = g->player->x - 4;//4 cases avant joueur
 	i = 0;
-	while (i < 9)
+	while (x < g->player->x + 4)// jusqua 4 cases apres 
 	{
 		y = g->player->y - 4;
 		j = 0;
-		while (j < 9) 
+		while (y < g->player->y + 4)
 		{
-			if (x < 0 || y < 0 || g->map->size_x < x || g->map->size_y < y)
-			{
-				j++;
-				y++;
-				continue;
-			}
-			else if (x >= 0 && y >= 0 && x < g->map->size_x && y < g->map->size_y && g->map->map[y][x] == '1')
-				ft_print_square(i + 1, j + 1, minimap, 0x2f00ff);
+			if (x >= 0 && y >= 0 && x < g->map->size_x && y < g->map->size_y && g->map->map[y][x] == '1')
+				ft_print_square(i, j, minimap, 0x2f00ff);
 			y++;
 			j++;
 		}
 		x++;
 		i++;
 	}
-	ft_print_square(5, 5, minimap, 0xff00b7);
+	ft_print_square(4, 4, minimap, 0xff00b7);
 }
