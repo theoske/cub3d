e5a36fba2ab67d8f9d8b00c855fcc687dbb3620e
Theo Kempf elbaz <tkempf-e@k0r3p12.42mulhouse.fr>
2023-01-19 17:58:43 +0100

    smooth minimap

--- srcs/bonus/ft_minimap.c
+++ srcs/bonus/ft_minimap.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:39:01 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/19 14:32:46 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/01/19 17:58:09 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,20 @@ static void	ft_print_square(double x, double y, mlx_image_t *minimap, uint32_t c
 {
 	double	i;
 	double	j;
-	double	square_size[2];
+	double	square_size;
 	int	scale;
 
 	scale = SCREEN_X;
 	if (SCREEN_X > SCREEN_Y)
 		scale = SCREEN_Y;
-	square_size[0] = (double)(scale / 9 / 4);
-	square_size[1] = (double)(scale / 9 / 4);
-	i = square_size[0] * x;
-	while (i < (square_size[0] * x) + square_size[0])
+	square_size = (double)(scale / 9 / 4);
+	i = square_size * x;
+	while (i < (square_size * x) + square_size)
 	{
-		j = square_size[1] * y;
-		while (j < (square_size[1] * y) + square_size[1])
+		j = square_size * y;
+		while (j < (square_size * y) + square_size)
 		{
-			if (i >= 0 && j >= 0)
+			if (i >= 0 && j >= 0 && j <= scale / 4 - square_size && i <= scale / 4 - square_size)
 				mlx_put_pixel(minimap, i, j, color);
 			j++;
 		}
@@ -48,12 +47,37 @@ static void	ft_mini_init(mlx_image_t *minimap)
 	if (SCREEN_X > SCREEN_Y)
 		scale = SCREEN_Y;
 	i = 0;
-	while (i < scale / 4)
+	while (i < scale / 4 - (scale / 4 / 9))
 	{
 		j = 0;
-		while (j < scale / 4)
+		while (j < scale / 4 - (scale / 4 / 9))
 		{
 			mlx_put_pixel(minimap, i, j, 0x0000ff);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_print_player(double x, double y, mlx_image_t *minimap, uint32_t color)
+{
+	double	i;
+	double	j;
+	double	square_size;
+	int	scale;
+
+	scale = SCREEN_X;
+	if (SCREEN_X > SCREEN_Y)
+		scale = SCREEN_Y;
+	square_size = (double)(scale / 9 / 4);
+	i = (square_size * x);
+	while (i < (square_size * x) + (square_size * 0.10))
+	{
+		j = (square_size * y);
+		while (j < (square_size * y) + (square_size * 0.10))
+		{
+			if (i >= 0 && j >= 0)
+				mlx_put_pixel(minimap, i, j, color);
 			j++;
 		}
 		i++;
@@ -72,15 +96,15 @@ void	ft_minimap(t_game *g)
 	ft_mini_init(minimap);
 	x = g->player->x - 4;
 	i = 0;
-	while (x < g->player->x + 4)
+	while (x < g->player->x + 5)
 	{
 		y = g->player->y - 4;
 		j = 0;
-		while (y < g->player->y + 4)
+		while (y < g->player->y + 5)
 		{
-			if (x >= 0 && y >= 0 && x < g->map->size_x && y < g->map->size_y && g->map->map[y][x] != '0' && g->map->map[y][x] != 'D' && g->map->map[y][x] != 'O')
+			if (x >= 0 && y >= 0 && x < g->map->size_x && y < g->map->size_y && g->map->map[y][x] != '0' && g->map->map[y][x] != 'D')
 				ft_print_square(i + ((int)g->player->x - g->player->x), j + ((int)g->player->y - g->player->y), minimap, 0x2f00ff);
-			else if (x >= 0 && y >= 0 && x < g->map->size_x && y < g->map->size_y && (g->map->map[y][x] == 'D' || g->map->map[y][x] == 'O'))
+			else if (x >= 0 && y >= 0 && x < g->map->size_x && y < g->map->size_y && g->map->map[y][x] == 'D')
 				ft_print_square(i + ((int)g->player->x - g->player->x), j + ((int)g->player->y - g->player->y), minimap, 0xffffff);
 			y++;
 			j++;
@@ -88,5 +112,5 @@ void	ft_minimap(t_game *g)
 		x++;
 		i++;
 	}
-	ft_print_square(i - 5, j - 5, minimap, 0xff00b7);//mettre joueur plus petit (croix?)
+	ft_print_player(i - 6, j - 6, g->img, 0xff00b7);
 }
